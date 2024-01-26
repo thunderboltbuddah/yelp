6e72cdb8f8f4053dbc4cff91337f96b83c38f591
unknown <aownmuhammad070@gmail.com>
2024-01-26 14:32:06 +0500

    added top nav

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopNav from './TopNav/TopNav'
+import logo from '../assets/logo.png'
+function LandingPage() {
+  return (
+    <div>
+        <TopNav/>
+<img src={logo} className='sty'>
+</img>
+       </div>
+  )
+}
+
+export default LandingPage
---
+++ src/LandingPage/TopNav/TopNav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './TopNav.module.css'
+function TopNav() {
+  return (
+    <div className={styles['top-nav']}>
+        <div className={styles['left']} >
+        <span >
+            Write a Review
+        </span>
+        <span>
+        Events
+        </span>
+        </div>
+        <div  className={styles['right']}>
+        <span >
+            Login
+        </span>
+        <button className='button'>
+Sign Up
+        </button>
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default TopNav
